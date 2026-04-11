077c764d17c99b3ed183e1d9f48954772bef0abf
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2026-04-11 14:33:17 +0900

    移行作業を明記

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,13 @@ export default function Home() {
     <>
       <Header />
       <main className="p-2">
+        <p className="block p-4 text-center text-2xl">
+          ※ このサイトはまもなく
+          <a href="https://chakkun1121.com" className="text-blue-600 underline">
+            https://chakkun1121.com
+          </a>
+          に移行します。ただいま移行作業中です。
+        </p>
         <Info />
         <div className="mx-auto max-w-4xl justify-center gap-12 md:flex">
           <div className="flex-1">
